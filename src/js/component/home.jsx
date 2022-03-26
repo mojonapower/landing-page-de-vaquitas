d0913e9a34cbfd230138f2e8d83d63c8d199b0c9
mojonapower <maca95@gmail.com>
2022-03-26 00:07:41 +0000

    vaquitas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-
+import Navbar from "./navbar.jsx";
+import Card from "./card.jsx";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<Navbar titulo="Mi Super Navbar" />
+			<div className="row">
+				<Card
+					img="http://www.masquesaludanimal.es//posts/asset_upload_file89331_522110.jpg"
+					titulo="Mira! es peludita"
+				/>
+				<Card
+					img="https://dkt6rvnu67rqj.cloudfront.net/sites/default/files/styles/max_1000/public/media/cr_files/nariz.jpg?itok=l93S3p1h"
+					titulo="Una vaca tradicional"
+				/>
+			</div>
 		</div>
 	);
 };
